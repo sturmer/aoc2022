a500772811b5f893e76629f7df5e0efd99992839
Gianluca Ciccarelli <gian@ciccarelli.dev>
2022-12-09 18:36:18 +0200

    Solve day 1

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,6 +1,6 @@
 defmodule Aoc2022.Day01 do
   def solve_part_one do
-    case File.read("input/day01.txt") do
+    case File.read("input/day01.sample.txt") do
       {:ok, content} -> content
       |> String.split("\n")
       |> find_max_cal
@@ -21,5 +21,23 @@ defmodule Aoc2022.Day01 do
 
   def to_lists_of_ints([], res) do
     res
+  end
+
+  def solve_part_two do
+    case File.read("input/day01.txt") do
+      {:ok, content} -> content
+      |> String.split("\n")
+      |> find_max_sum_cal
+    end
+  end
+
+  def find_max_sum_cal(lst) do
+    Enum.chunk_by(lst, &(&1 == ""))
+    |> Enum.reject(&(Enum.at(&1, 0) == "" && Kernel.length(&1) == 1))
+    |> to_lists_of_ints([])
+    |> Enum.map(fn x -> Enum.sum(x) end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum
   end
 end
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -6,4 +6,8 @@ defmodule Day01Test do
   test "part 1" do
     assert find_max_cal(["1", "2", "3", "", "4"]) == 6
   end
+
+  test "part 2" do
+    assert find_max_sum_cal(["1", "2", "3", "", "4", "", "1", "2", "4", "", "3"]) == 17
+  end
 end
