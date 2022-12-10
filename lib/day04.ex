69889fc47f12e575345d62d2bbf2a66b14223831
Gianluca Ciccarelli <gian@ciccarelli.dev>
2022-12-10 14:11:05 +0200

    Solve day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,16 +1,21 @@
 defmodule Day04 do
-  def solve_part_one(file) do
+  def solve(file, part) do
+    test_fun = case part do
+      :one -> &is_full_inclusion?/1
+      :two -> &is_overlap?/1
+    end
+
     case File.read(file) do
       {:ok, content} ->
         content
         |> String.split("\n", trim: true)
-        |> count_full_inclusions()
+        |> count_overlaps(test_fun)
     end
   end
 
-  def count_full_inclusions(pairs) do
+  def count_overlaps(pairs, test_fun) do
     pairs
-    |> Enum.reduce(0, fn pair, acc -> acc + if is_full_inclusion?(pair), do: 1, else: 0 end)
+    |> Enum.reduce(0, fn pair, acc -> acc + if test_fun.(pair), do: 1, else: 0 end)
   end
 
   @doc """
@@ -18,11 +23,21 @@ defmodule Day04 do
     false
   """
   def is_full_inclusion?(pair) do
-    [one, two] = String.split(pair, ",", trim: true)
+    [r1, r2] = pair_to_mapsets(pair)
+    MapSet.subset?(r1, r2) || MapSet.subset?(r2, r1)
+  end
+
+  def is_overlap?(pair) do
+    [r1, r2] = pair_to_mapsets(pair)
+    (MapSet.intersection(r1, r2) |> MapSet.size()) > 0
+  end
+
+  def pair_to_mapsets(p) do
+    [one, two] = String.split(p, ",", trim: true)
     r1 = one |> normalize_range() |> MapSet.new()
     r2 = two |> normalize_range() |> MapSet.new()
 
-    MapSet.subset?(r1, r2) || MapSet.subset?(r2, r1)
+    [r1, r2]
   end
 
   @doc """
--- test/day04_test.exs
+++ test/day04_test.exs
@@ -4,7 +4,12 @@ defmodule Day04Test do
   import Day04
 
   test "part 1" do
-    assert solve_part_one("input/day04.sample.txt") == 2
-    assert solve_part_one("input/day04.txt") == 498
+    assert solve("input/day04.sample.txt", :one) == 2
+    assert solve("input/day04.txt", :one) == 498
+  end
+
+  test "part 2" do
+    assert solve("input/day04.sample.txt", :two) == 4
+    assert solve("input/day04.txt", :two) == 859
   end
 end
